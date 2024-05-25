d16f01fa72226a2da84c94c9c5d9663ed6d7043e
alex <alexgeiser78@gmail.com>
2024-05-25 20:32:35 +0200

    reexecute

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -16,13 +16,12 @@
 int	ft_execute(t_scene *scene)
 {
 	printf("IN FT_EXECUTE\n");
-	// (void)scene;
-	// void	*mlx_ptr;
-	//void	*window_ptr;
-	// mlx_ptr = malloc(sizeof(void *));
+	//(void)scene;
+
+	scene->mlx_ptr = malloc(sizeof(void *));
 	scene->mlx_ptr = mlx_init();
 	scene->window_ptr = mlx_new_window(scene->mlx_ptr, 1000, 700, "miniRT");
-	//mlx_loop(scene->mlx_ptr);
+	mlx_loop(scene->mlx_ptr);
 	printf("OUT FT_EXECUTE\n");
 	return (0);
 }
