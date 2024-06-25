3c630128e50888af559c9e0a0cf674eaf2d157f0
Alexandre Geiser <ageiser@car9s2.42barcelona.com>
2024-06-25 16:32:40 +0200

    clean

--- src/execute/inter_sphere.c
+++ src/execute/inter_sphere.c
@@ -26,7 +26,6 @@ void	ft_inter_sp(t_intersection *inter, t_element *c_element)
 	t_vector transformed_direction = inter->ray.direction;
 	//transformed_direction = normalize(transformed_direction);//
 
-	
 	//printf("OC: (%f, %f, %f, %f, %f)\n", oc.x, oc.y, oc.z, oc.length_squared, oc.length);
 
 	// printf("entro en inter_sp.....\n");
