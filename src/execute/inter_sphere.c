fe52a52af66ed736a272c924e67edee52076866b
Alexandre Geiser <ageiser@car8s2.42barcelona.com>
2024-06-26 13:31:56 +0200

    shadow cut

--- src/execute/inter_sphere.c
+++ src/execute/inter_sphere.c
@@ -55,7 +55,7 @@ void ft_inter_sp(t_intersection *inter, t_element *c_element) //including the ma
 
    ///PAULA'S CODE commentend
 
-/*
+
 void	ft_inter_sp(t_intersection *inter, t_element *c_element)
 {
 	t_vector	oc;
@@ -65,9 +65,9 @@ void	ft_inter_sp(t_intersection *inter, t_element *c_element)
 	float		disc;
 	float	t;
 
-	t_matrix translation_matrix = create_translation_matrix(-c_element->position.x, -c_element->position.y, -c_element->position.z);
-	t_vector transformed_origin = apply_matrix(translation_matrix, inter->ray.origin);
-	t_vector transformed_direction = inter->ray.direction;
+	//t_matrix translation_matrix = create_translation_matrix(-c_element->position.x, -c_element->position.y, -c_element->position.z);
+	//t_vector transformed_origin = apply_matrix(translation_matrix, inter->ray.origin);
+	//t_vector transformed_direction = inter->ray.direction;
 	//transformed_direction = normalize(transformed_direction);//
 	//(void)transformed_origin;
 	//(void)transformed_direction;
@@ -75,12 +75,12 @@ void	ft_inter_sp(t_intersection *inter, t_element *c_element)
 	//printf("OC: (%f, %f, %f, %f, %f)\n", oc.x, oc.y, oc.z, oc.length_squared, oc.length);
 
 	// printf("entro en inter_sp.....\n");
-	oc = ft_sub_vectors( (t_vector){0, 0, 0, 0, 0}, transformed_origin );// Centre de la sphère est maintenant à l'origine
-	//oc = ft_sub_vectors(c_element->position, inter->ray.origin);
-	a = ft_get_length_squared(transformed_direction);
-	//a = ft_get_length_squared(inter->ray.direction);
-	h = ft_dot(transformed_direction, oc);
-	//h = ft_dot(inter->ray.direction, oc);
+	//oc = ft_sub_vectors( (t_vector){0, 0, 0, 0, 0}, transformed_origin );// Centre de la sphère est maintenant à l'origine
+	oc = ft_sub_vectors(c_element->position, inter->ray.origin);
+	//a = ft_get_length_squared(transformed_direction);
+	a = ft_get_length_squared(inter->ray.direction);
+	//h = ft_dot(transformed_direction, oc);
+	h = ft_dot(inter->ray.direction, oc);
 	// printf("h: %f\n", h);
 	c = ft_get_length_squared(oc) - ((c_element->diameter / 2.0) * (c_element->diameter / 2.0));
 	// printf("c: %f\n", c);
@@ -95,8 +95,8 @@ void	ft_inter_sp(t_intersection *inter, t_element *c_element)
 		}
 	}
 }
-*/
 
+/*
 void	ft_inter_sp(t_intersection *inter, t_element *c_element)
 {
 	t_vector	oc;
@@ -124,7 +124,7 @@ void	ft_inter_sp(t_intersection *inter, t_element *c_element)
 			ft_get_inter_sp(inter, c_element, t);
 		}
 	}
-}
+}*/
 
 
 void	ft_get_closest_point(t_intersection *inter, t_vector inter_point, t_element *c_element)
@@ -169,3 +169,34 @@ void	ft_get_inter_sp(t_intersection *inter, t_element *c_element, float t)
 		ft_get_closest_point(inter, inter_point, c_element);
 	}
 }
+/*
+void ft_inter_sp(t_intersection *inter, t_element *c_element)
+{
+    t_vector oc;
+    float a, h, c, disc, t;
+
+    t_matrix translation_matrix = create_translation_matrix(-c_element->position.x, -c_element->position.y, -c_element->position.z);
+    t_vector transformed_origin = apply_matrix(translation_matrix, inter->ray.origin);
+    t_vector transformed_direction = inter->ray.direction;
+    
+    oc = ft_sub_vectors((t_vector){0, 0, 0, 0, 0}, transformed_origin);
+    a = ft_get_length_squared(transformed_direction);
+    h = ft_dot(transformed_direction, oc);
+    c = ft_get_length_squared(oc) - ((c_element->diameter / 2.0) * (c_element->diameter / 2.0));
+    disc = h * h - a * c;
+
+    if (disc >= 0)
+    {
+        t = (h - sqrtf(disc)) / a;
+        t_vector inter_point = ft_add_vectors(inter->ray.origin, ft_mult_vector_float(inter->ray.direction, t));
+        float distance = ft_get_vector_length(ft_sub_vectors(inter_point, inter->ray.origin));
+
+        if (inter->state == 0 || distance < inter->distance)
+        {
+            inter->position = inter_point;
+            inter->element = c_element;
+            inter->distance = distance;
+            inter->state = 1;
+        }
+    }
+}*/
