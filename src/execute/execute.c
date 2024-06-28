1dc0c30593573ff4098beefe75d3732ae7fb6a58
Alexandre Geiser <ageiser@car7s1.42barcelona.com>
2024-06-28 14:34:26 +0200

    norm

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -15,57 +15,49 @@
 // TO DO ONCE PARSING IS DONE
 int	ft_execute(t_scene *scene)
 {
-	int	i = 0;
-	int	j = 0;
-	// t_ray	ray;
+	int				i;
+	int				j;
+	int				start_x;
+	int				start_y;
 	t_intersection	*inter;
-	
+
+	j = 0;
 	inter = malloc(sizeof(t_intersection));
-	ft_get_viewport(scene);   //PAULA
-	// ft_print_viewport(scene);
-	 mlx_initiator(scene);
-	 hook_init(scene);
-	 pixel_print(scene);
+	ft_get_viewport(scene);
+	mlx_initiator(scene);
+	hook_init(scene);
+	pixel_print(scene);
 	while (j < scene->image_side)
 	{
 		i = 0;
 		while (i < scene->image_side)
 		{
 			inter->state = 0;
-			ft_get_ray(scene, inter, j, i);	//PAULA
-			//ft_print_ray(inter->ray);
+			ft_get_ray(scene, inter, j, i);
 			ft_hit_something(scene, inter);
-			// if (inter->state == 1)
-			// 	ft_print_intersection(inter);
-			// else
-			// 	printf("nothing\n");
-			if (inter->state == 1)	//PAULA
-			{
-				//ft_pixel_put(&scene->img, i, j, );
-				//printf("inter->element->color.r: %d\n", inter->element->color.r);
-
-				//color_screen(scene, inter->element->color.r);
-				write_pixel_object(scene, inter, j, i); //ALEX
-				//printf("write_pixel_object\n");
-			}
+			if (inter->state == 1)
+				write_pixel_object(scene, inter, j, i);
 			else
-			{
-				write_pixel_no_object(scene); //ALEX
-				//color_screen(scene, 0xff00);
-				//printf("write_pixel_no_object\n");
-			}
-			
+				write_pixel_no_object(scene);
 			i++;
 		}
 		j++;
 	}
-	int start_x = (WIN_WIDTH - scene->image_side) / 2;
-    int start_y = (WIN_HEIGHT - scene->image_side) / 2;  
-    mlx_put_image_to_window(scene->mlx_ptr, scene->window_ptr, scene->img.img_ptr, start_x, start_y);
-	printf("img2win\n");
+	start_x = (WIN_WIDTH - scene->image_side) / 2;
+	start_y = (WIN_HEIGHT - scene->image_side) / 2;
+	mlx_put_image_to_window(scene->mlx_ptr, scene->window_ptr, scene->img.img_ptr, start_x, start_y);
 	mlx_loop(scene->mlx_ptr);
-	//free(inter);
-	printf("OUT FT_EXECUTE\n");
 	return (0);
 }
-
+	//free(inter);
+	//printf("OUT FT_EXECUTE\n");
+	//printf("img2win\n");
+	//color_screen(scene, 0xff00);
+	//printf("write_pixel_no_object\n");
+	// if (inter->state == 1)
+	// 	ft_print_intersection(inter);
+	// else
+	// 	printf("nothing\n");
+	//ft_print_ray(inter->ray);
+	// ft_print_viewport(scene);
+	// t_ray	ray;
