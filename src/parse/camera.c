7acd4d964e6bae4ef6e13979c94ba5fff63a9405
Alexandre Geiser <ageiser@car7s1.42barcelona.com>
2024-06-28 13:33:46 +0200

    norm

--- src/parse/camera.c
+++ src/parse/camera.c
@@ -12,39 +12,35 @@
 
 #include "minirt.h"
 
-int	 ft_get_camera(char **params, t_scene *scene)
+int	ft_get_camera(char **params, t_scene *scene)
 {
 	print_in_out("IN FT_GET_CAMERA\n");
-	// check if camera is already defined
 	if (scene->camera.defined == 0)
 	{
 		scene->camera.defined = 1;
-		// if it's not defined, ft_data_camera stores data in struc scene
 		if (ft_data_camera(params, scene) == 1)
 		{
 			perror("error: camera: wrong parameters\n");
 			print_in_out("OUT FT_GET_CAMERA IN 1\n");
 			return (1);
 		}
-		// ft_print_camera, _ambient, ft_print_light are debug functions
-
 		print_in_out("OUT FT_GET_CAMERA IN 2\n");
 		return (0);
 	}
 	else
 	{
-		// if camera is already defined, error
 		perror("error: camera: camera already defined\n");
 		return (1);
 	}
 }
-
-
+	// ft_print_camera, _ambient, ft_print_light are debug functions
+	// if camera is already defined, error
+	// if it's not defined, ft_data_camera stores data in struc scene
+	// check if camera is already defined
 
 int	ft_data_camera(char **params, t_scene *scene)
 {
 	print_in_out("IN FT_DATA_CAMERA\n");
-	// params in camera must be 4: identifier, pov, orientation and fov
 	if (ft_count_params(params) != 4)
 	{
 		print_in_out("OUT FT_DATA_CAMERA IN 1\n");
@@ -69,32 +65,32 @@ int	ft_data_camera(char **params, t_scene *scene)
 	return (0);
 }
 
+// params in camera must be 4: identifier, pov, orientation and fov
+
 int	ft_get_fov(char **params, t_scene *scene)
 {
 	print_in_out("IN FT_GET_FOV\n");
-	// fov is a positive int in the range 0-180 (0 not included, I think)
 	if (ft_is_positive(params[3]) == 1 && ft_atoi(params[3]) > 0
 		&& ft_atoi(params[3]) < 181)
 	{
 		scene->camera.fov = ft_atoi(params[3]);
 		print_in_out("OUT FT_GET_FOV WITH FOV \n");
 		return (0);
-
 	}
 	print_in_out("OUT FT_GET_FOV WITHOUT FOV\n");
 	return (1);
 }
 
+	// fov is a positive int in the range 0-180 (0 not included, I think)
+
 int	ft_get_orientation(char **params, t_scene *scene)
 {
-	print_in_out("IN FT_GET_ORIENTATION\n");
-	char **co;
+	char	**co;
 
+	print_in_out("IN FT_GET_ORIENTATION\n");
 	co = ft_split(params[2], ',');
-	// orientation must be a normalized vector
 	if (ft_is_vector(co) == 1 && ft_is_normalized(co) == 1)
 	{
-		// stores orientation in struct scene
 		scene->camera.orientation.x = ft_atof(co[0]);
 		scene->camera.orientation.y = ft_atof(co[1]);
 		scene->camera.orientation.z = ft_atof(co[2]);
@@ -102,19 +98,19 @@ int	ft_get_orientation(char **params, t_scene *scene)
 		ft_free_params(co);
 		return (0);
 	}
-	// if co is not a normalized vector, error
 	ft_free_params(co);
 	print_in_out("OUT FT_GET_ORIENTATION IN 2\n");
 	return (1);
 }
+	// orientation must be a normalized vector
+	// stores orientation in struct scene
+	// if co is not a normalized vector, error
 
 int	ft_is_vector(char **co)
 {
-	// check if co is a vector
-	print_in_out("IN FT_IS_VECTOR\n");
 	int	i;
 
-	// co must have 3 parameters
+	print_in_out("IN FT_IS_VECTOR\n");
 	if (ft_count_params(co) != 3)
 	{
 		print_in_out("OUT FT_IS_VECTOR, PARAMS != 3\n");
@@ -123,7 +119,6 @@ int	ft_is_vector(char **co)
 	i = 0;
 	while (co[i])
 	{
-		// each parameter must be a float
 		if (ft_is_float(co[i]) == 0)
 		{
 			print_in_out("OUT FT_IS_VECTOR, NO FLOATS\n");
@@ -134,4 +129,6 @@ int	ft_is_vector(char **co)
 	print_in_out("OUT FT_IS_VECTOR, THEY ARE FLOATS\n");
 	return (1);
 }
-
+		// check if co is a vector
+		// each parameter must be a float
+		// co must have 3 parameter
