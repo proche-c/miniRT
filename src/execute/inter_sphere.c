b3b791e84bf5de10415c4f988b9059e0a4aac58c
Paula Roche castro <proche-c@car5s4.42barcelona.com>
2024-07-04 13:25:02 +0200

    lk

--- src/execute/inter_sphere.c
+++ src/execute/inter_sphere.c
@@ -24,18 +24,17 @@ float *h)
 	return ((*h * *h) - (a * c));
 }
 
-float	calculate_t(float h, float disc, float a)
+float calculate_t(float h, float disc, float a)
 {
-	float	t1;
-	float	t2;
+    float t1 = (h - sqrtf(disc)) / a;
+    float t2 = (h + sqrtf(disc)) / a;
 
-	t1 = h - sqrtf(disc) / a;
-	t2 = h + sqrtf(disc) / a;
-	if (t1 < t2)
-		return (t1);
-	else
-		return (t2);
-	// return ((h - sqrtf(disc)) / a);
+    if (t1 > 0.001 && t2 > 0.001)
+        return (t1 < t2) ? t1 : t2;
+    else if (t1 > 0.001)
+        return t1;
+    else
+        return t2;
 }
 
 void	ft_inter_sp(t_intersection *inter, t_element *c_element)
@@ -84,19 +83,9 @@ t_vector inter_point, t_element *c_element)
 {
 	float		length1;
 	float		length2;
-	// t_vector	v1;
-	// t_vector	v2;
-
-	// v1 = ft_sub_vectors(inter_point, inter->ray.pixel_center);
-	// length1 = ft_get_vector_length(v1);
-	// v2 = ft_sub_vectors(inter->position, inter->ray.pixel_center);
-	// length2 = ft_get_vector_length(v2);
 
 	length1 = ft_distance(inter->ray.origin, inter_point);
 	length2 = ft_distance(inter->ray.origin, inter->position);
-	printf("inter->ray.direction.x: %f, inter->ray.direction.y: %f, inter->ray.direction.z: %f\n", inter->ray.direction.x, inter->ray.direction.y, inter->ray.direction.z);
-	printf("inter_point.x: %f, inter_point.y: %f, inter_point.z: %f\n", inter_point.x, inter_point.y, inter_point.z);
-	printf("inter->position.x: %f, inter->position.y: %f, inter->position.z: %f\n", inter->position.x, inter->position.y, inter->position.z);
 	if (length1 < length2)
 	{
 		inter->position.x = inter_point.x;
@@ -105,18 +94,16 @@ t_vector inter_point, t_element *c_element)
 		inter->element = c_element;
 		inter->distance = ft_distance(inter->ray.origin, inter_point);
 	}
-	// printf("inter_point.x: %f, inter_point.y: %f, inter_point.z: %f\n", inter_point.x, inter_point.y, inter_point.z);
-	// printf("inter->position.x: %f, inter->position.y: %f, inter->position.z: %f\n", inter->position.x, inter->position.y, inter->position.z);
-	printf("length1: %f and length2: %f, inter->element: %s\n", length1, length2, inter->element->identifier);
 }
 
 void	ft_get_inter_sp(t_intersection *inter, t_element *c_element, float t)
 {
 	t_vector	inter_point;
 
-	inter_point = ft_mult_vector_float(inter->ray.direction, t);
-	inter_point = ft_add_vectors(inter->ray.origin, inter_point);
-	if (inter->state == 0 || t < inter->distance)
+	inter_point.x = inter->ray.origin.x + inter->ray.direction.x * t;
+	inter_point.y = inter->ray.origin.y + inter->ray.direction.y * t;
+	inter_point.z = inter->ray.origin.z + inter->ray.direction.z * t;
+	if (inter->state == 0)
 	{
 		inter->position.x = inter_point.x;
 		inter->position.y = inter_point.y;
@@ -130,4 +117,3 @@ void	ft_get_inter_sp(t_intersection *inter, t_element *c_element, float t)
 		ft_get_closest_point(inter, inter_point, c_element);
 	}
 }
-	//printf("inter->distace = %f\n", inter->distance); //
